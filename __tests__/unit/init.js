4ee01bb37ccd2df3f340c6452ec50d529ec58de9
Levi Trammell <davidltrammell@gmail.com>
2021-02-15 11:07:39 -0600

    Fix lint for real this time

--- __tests__/unit/init.js
+++ __tests__/unit/init.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 jest.mock('../../node_modules/react-native/Libraries/EventEmitter/NativeEventEmitter');
 
 jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
