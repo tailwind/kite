e58ac3d32d338a186576a8bf88aaf0b45e4cd317
Levi Trammell <davidltrammell@gmail.com>
2021-02-15 15:19:32 -0600

    Add enableScreens to App.tsx

--- src/domains/core/App.tsx
+++ src/domains/core/App.tsx
@@ -2,8 +2,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import React, { FC } from 'react';
 import { ActivityIndicator } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { enableScreens } from 'react-native-screens';
 import { useNavigationRestore } from 'src/domains/core/hooks/useNavigationRestore';
 import { RootStackNavigator } from 'src/domains/core/screens/RootStackNavigator';
+
+enableScreens();
 
 export const App: FC = () => {
   const { isReady, initialState, onStateChange } = useNavigationRestore();
