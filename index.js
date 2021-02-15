b3922348662772d7fcc379e59a0d9fc4369d0318
Levi Trammell <davidltrammell@gmail.com>
2021-02-15 15:45:20 -0600

    Add wdyr to index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
+import { config } from './src/config';
 import { App } from './src/domains/core/App';
+
+// eslint-disable-next-line no-undef
+if (__DEV__ && config.whyDidYouRender) {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  const ReactRedux = require('react-redux');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+    trackExtraHooks: [[ReactRedux, 'useSelector']],
+  });
+}
 
 AppRegistry.registerComponent(appName, () => App);
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
 interface Config {
   usesNavigationRestore: boolean;
+  whyDidYouRender: boolean;
 }
 
 export const config: Config = {
   usesNavigationRestore: __DEV__ && process.env.JEST_WORKER_ID === undefined,
+  whyDidYouRender: __DEV__,
 };
