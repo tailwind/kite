fb91ae16ca8c39928fe97c508ef0c284dba103f4
Garrett Cox <garrettjcox@gmail.com>
2021-03-02 11:22:29 -0600

    Adjust lint ignore

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
 
 import React from 'react';
@@ -7,8 +6,8 @@ import { name as appName } from './app.json';
 import { config } from './src/config';
 import { App } from './src/domains/core/App';
 
-// eslint-disable-next-line no-undef
 if (__DEV__ && config.whyDidYouRender) {
+  // eslint-disable-next-line import/no-extraneous-dependencies
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   const ReactRedux = require('react-redux');
   whyDidYouRender(React, {
