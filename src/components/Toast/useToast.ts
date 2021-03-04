0369f06d7a5d691158e961106fd020f0a80a48b6
Sam Fletcher <sfletchertx@gmail.com>
2021-03-03 21:25:58 -0600

    Simplify useToast

--- src/components/Toast/useToast.ts
+++ src/components/Toast/useToast.ts
@@ -2,8 +2,4 @@ import { useContext } from 'react';
 import { ToastContext } from 'src/components/Toast/toastContext';
 import { ToastOptions } from 'src/components/Toast/types';
 
-export const useToast = (): ToastOptions => {
-  const { toast, displayToast, closeToast } = useContext(ToastContext);
-
-  return { toast, displayToast, closeToast };
-}
+export const useToast = (): ToastOptions => useContext(ToastContext)
